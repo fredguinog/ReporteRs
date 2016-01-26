69f49349886ee770a62b161ccf4844abe8463629
David Gohel <david.gohel@ardata.fr>
2016-01-26 13:03:21 +0100

    update doc

--- rexamples/powerpoint_plot_fontname.R
+++ rexamples/powerpoint_plot_fontname.R
@@ -4,24 +4,21 @@ library( ReporteRs )
 pptdoc = pptx( )
 pptdoc = addSlide( pptdoc, "Blank" )
 
-myggplot = qplot(Sepal.Length, Petal.Length, data = iris, color = Species, size = Petal.Width )
+myggplot = qplot(Sepal.Length, Petal.Length, data = iris,
+                 color = Species, size = Petal.Width )
 
 # use Helvetica as font
-pptdoc = addPlot(
-  doc = pptdoc,
-  fun = print,
+pptdoc = addPlot( doc = pptdoc, fun = print,
   offx = 1, offy = 1, width = 5, height = 5,
   x = myggplot + ggtitle("fontname Helvetica"),
   vector.graphic=TRUE,
   fontname_serif = "Helvetica")
 
 # use Courier New as font
-pptdoc = addPlot(
-  doc = pptdoc,
-  fun = print,
+pptdoc = addPlot( doc = pptdoc, fun = print,
   offx = 6, offy = 1, width = 5, height = 5,
   x = myggplot + ggtitle("fontname Courier New")
- , vector.graphic=TRUE
- , fontname_serif = "Courier New" )
+  , vector.graphic=TRUE
+  , fontname_serif = "Courier New" )
 
 writeDoc( pptdoc, "examples/pp_plot_fontname.pptx" )
